f5d4269b7ae90a117f66a449c36fe63c40759635
Alex Labonne <>
2020-08-09 18:46:08 +0200

    Updates

---
+++ precache-manifest.0345590669cbaf73bb9de810edc95642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3249cdff3724ef87cc9b87ac7f276a",
+    "url": "/github-issuesg/index.html"
+  },
+  {
+    "revision": "da3a5afe6c4d87c63aa0",
+    "url": "/github-issuesg/static/css/main.d73d045b.chunk.css"
+  },
+  {
+    "revision": "7750d516edd71ede3d2b",
+    "url": "/github-issuesg/static/js/2.da51f37a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-issuesg/static/js/2.da51f37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3a5afe6c4d87c63aa0",
+    "url": "/github-issuesg/static/js/main.7ce80724.chunk.js"
+  },
+  {
+    "revision": "5dd0e7a0f6645249d1d9",
+    "url": "/github-issuesg/static/js/runtime-main.4c66be25.js"
+  }
+]);
